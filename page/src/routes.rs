5d3f86470d656ea74d9293b9fb25f10dadb26908
Arief Novianto <arief.novianto@varx.id>
2025-07-28 09:18:29 +0700

    update inertia

--- page/src/routes.rs
+++ page/src/routes.rs
@@ -1,11 +1,11 @@
 use actix_web::{web};
 use inertia_rust::{InertiaService};
-use super::index::render_index;
-use super::contact::render_contact;
+use super::index;
+use super::contact;
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(render_index)
-        .service(render_contact)
+    cfg.service(index::render_index)
+        .service(contact::render_contact)
         .inertia_route("/todo", "Todo/Index")
         .inertia_route("/todo/create", "Todo/Create");
 }
