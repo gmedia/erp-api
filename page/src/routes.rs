692ad29cc4a2f3294216f06ae8f434d8b0e2ac8c
Arief Novianto <arief.novianto@varx.id>
2025-07-28 10:55:07 +0700

    update inertia

--- page/src/routes.rs
+++ page/src/routes.rs
@@ -1,12 +1,14 @@
-use actix_web::{web};
+use actix_web::web;
 use inertia_rust::{InertiaService};
 use super::v1::{index, contact, todo};
 use super::middlewares::reflash_temporary_session::ReflashTemporarySessionMiddleware;
 
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
-    cfg.wrap(ReflashTemporarySessionMiddleware::new())
+    cfg.service(
+        web::wrap(ReflashTemporarySessionMiddleware::new())
         .service(index::routes::render)
         .service(contact::routes::render)
         .service(todo::routes::render)
-        .inertia_route("/todo/create", "Todo/Create");
+        .inertia_route("/todo/create", "Todo/Create")
+    );
 }
